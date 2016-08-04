8abd363a6980340b426f462179aae0e8f234f583
Paul Adams <Paul Adams>
2016-08-04 16:23:22 -0400

    initial commit

---
+++ counter-example/src/App.vue
@@ -0,0 +1,28 @@
+
+<script>
+import Counter from './Counter.vue'
+export default {
+  data(){
+    return{
+    }
+  },
+  render(h){
+    const data = {
+      props:{
+        count: 0
+      }
+    }
+    return(
+      <div>
+        <Counter {...data}/>
+      </div>
+    )
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ counter-example/src/Counter.vue
@@ -0,0 +1,28 @@
+<script>
+    export default{
+        name: 'Counter',
+        props: ['count'],
+        data(){
+            return{
+                c : this.count
+            }
+        },
+        methods: {
+            inc(){
+                this.c++
+            },
+            dec(){
+                this.c--
+            }
+        },
+        render(h){
+            return(
+                <div>
+                    <h2>Count: {this.c}</h2>
+                    <button on-click={this.dec}>-</button>
+                    <button on-click={this.inc}>+</button>
+                </div>
+            )
+        }
+    }
+</script>
